2c79243f557ca42c05efeca31788f2f11161dc21
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-07-03 22:34:43 +0700

    feat: создать папку с экземплярами класса error

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const mustBeFilled = require('../helpers/mustBeFilled')
+const UnauthorizedError = require('../errors/UnauthorizedError')
 
 const userSchema = new mongoose.Schema({
   name: {
