7096b4c777bc55485c804e874e5dbf1a49ab0218
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-07-04 22:07:04 +0700

    feat: добавить контроллер фильмов и добавить его в соответствующие роуты

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const auth = require('./auth')
+const {
+  deleteMovie,
+  createMovie,
+  getCurrentUserMovies,
+} = require('../controllers/movies')
 
 router.use(auth)
 
-router.get('/',)
-router.post('/',)
-router.delete('/_id',)
+router.get('/', getCurrentUserMovies)
+router.post('/', createMovie)
+router.delete('/:id', deleteMovie)
 
 module.exports = router
