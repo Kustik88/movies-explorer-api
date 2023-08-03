8d51a9380887b6cde0aad11bc194de74f14538d1
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-08-03 11:23:14 +0600

    feat: разрешить корс для всех источников

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { errors } = require('celebrate')
 const cookieParser = require('cookie-parser')
-const corsOptions = require('./middlewares/cors')
 const rateLimiter = require('./middlewares/rateLimiter')
 const handlerError = require('./middlewares/handlerError')
 const router = require('./routes')
@@ -16,17 +15,12 @@ mongoose.connect(DB_ADDRESS)
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
 app.use(rateLimiter)
 app.use(express.json())
 app.use(helmet())
 app.use(requestLogger)
 app.use(cookieParser())
+app.use(cors())
 app.use(router)
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемая страница не найдена'))
@@ -34,9 +28,6 @@ app.use((req, res, next) => {
 app.use(errorLogger)
 app.use(errors())
 app.use(handlerError)
-app.get('/products/:id', cors(corsOptions), (req, res) => {
-  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' })
-})
 
 app.listen(PORT, () => {
   console.log(`Sever is running on port ${PORT}`)
