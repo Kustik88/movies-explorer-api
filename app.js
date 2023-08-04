53f52c2f8f247946d1ed0c1675b9881e154dbb42
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-08-04 14:48:45 +0600

    feat: разрешить корс для всех

--- app.js
+++ app.js
@@ -14,20 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger')
 mongoose.connect(DB_ADDRESS)
 
 const app = express()
-const whitelist = ['http://localhost:3000', 'http://kust-project.nomoreparties.sbs']
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-app.get('/products/:id', cors(corsOptionsDelegate), (req, res) => {
-  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' })
-})
+app.use(cors())
 app.use(rateLimiter)
 app.use(express.json())
 app.use(helmet())
